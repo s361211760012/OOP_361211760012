4393551d655ea31eab5b2b61885f6d228c7854c6
Monchanok Pinlawas <58647058+s361211760012@users.noreply.github.com>
2019-12-08 11:28:15 +0700

    Update Hello.java

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
         System.out.println("Saiyai");
-
+         System.out.println("Monchanok");
 
 
 
